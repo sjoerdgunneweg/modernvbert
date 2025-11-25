61b088bffbf305b568190fb0678d66814c8c86ad
JingfenQiao <jingfenqiao@gmail.com>
2025-11-25 11:50:38 +0100

    colpali

--- bash/train_colmodernvbert.sh
+++ bash/train_colmodernvbert.sh
@@ -8,8 +8,5 @@ accelerate launch --num_processes=1 --main_process_port 29501 modernvbert/src/mo
      -c modernvbert/src/modernvbert/contrastive_training/modernvbert/doc/config/colmodel_hardnegs_text_300k_no_vdr_from_base_sparse.yaml > log/colmodel_hardnegs_text_300k_no_vdr_from_base_sparse.log 2>&1 &
 
 
-accelerate launch --num_processes=1 --main_process_port 29504 modernvbert/src/modernvbert/contrastive_training/train.py \
-     -c modernvbert/src/modernvbert/contrastive_training/modernvbert/doc/config/colmodel_hardnegs_text_300k_no_vdr_from_base_sparse.yaml > log/colmodel_hardnegs_text_300k_no_vdr_from_base_sparse.log 2>&1 &
-
 
 # pip install dacite
