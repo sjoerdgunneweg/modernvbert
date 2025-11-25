95f9c477198ee0f3ac6232210b51088c39fd953b
JingfenQiao <jingfenqiao@gmail.com>
2025-11-25 11:39:01 +0100

    add lsr

--- bash/train_colmodernvbert.sh
+++ bash/train_colmodernvbert.sh
@@ -1,6 +1,4 @@
 
-
-
 accelerate launch --num_processes=2 --main_process_port 29500 modernvbert/src/modernvbert/contrastive_training/train.py \
      -c modernvbert/src/modernvbert/contrastive_training/modernvbert/doc/config/colmodel_hardnegs_text_300k_no_vdr_from_base.yaml > log/colmodel_hardnegs_text_300k_no_vdr_from_base.log 2>&1 &
 
