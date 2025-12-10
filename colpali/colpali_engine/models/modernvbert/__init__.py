5247cec5e40d4e921f17fb0d68b869ac71661860
k-maradiaga <ken.maradiaga.rosales@student.uva.nl>
2025-12-10 11:59:20 +0100

    fixed init

--- colpali/colpali_engine/models/modernvbert/__init__.py
+++ colpali/colpali_engine/models/modernvbert/__init__.py
@@ -2,3 +2,4 @@ from .bivbert import BiModernVBert, BiModernVBertProcessor
 from .colvbert import ColModernVBert, ColModernVBertProcessor
 from .mlm_sparse import ColModernVBertSparse, ColModernVBertSparseProcessor
 from .mlp_sparse import ColModernVBertMLPSparse, ColModernVBertMLPSparseProcessor
+from .m2 import ColModernVBertM2Wrapper, ColModernVBertM2Processor
