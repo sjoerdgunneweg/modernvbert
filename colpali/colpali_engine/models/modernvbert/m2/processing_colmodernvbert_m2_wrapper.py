f5f3a714fbebc056010c4f7833726f31c62098e6
k-maradiaga <ken.maradiaga.rosales@student.uva.nl>
2025-12-12 20:31:29 +0100

    Fixed wrong naming

--- colpali/colpali_engine/models/modernvbert/m2/processing_colmodernvbert_m2_wrapper.py
+++ colpali/colpali_engine/models/modernvbert/m2/processing_colmodernvbert_m2_wrapper.py
@@ -12,43 +12,44 @@ from ..mlm_sparse.processing_colmodernvbert_sparse import ColModernVBertSparsePr
 
 class ColModernVBertM2Processor(BaseVisualRetrieverProcessor, Idefics3Processor):
     """
-    Processor for M2
+    Processor for M2 (MLP for queries, MLM for documents)
     """
 
     def __init__(self, *args, image_seq_len=64, **kwargs):
         super().__init__(*args, image_seq_len=image_seq_len, **kwargs)
 
         self.query_processor = ColModernVBertMLPSparseProcessor(*args, **kwargs)
+        self.doc_processor   = ColModernVBertSparseProcessor(*args, **kwargs)
+    
+        self.query_augmentation_token = self.query_processor.query_augmentation_token
+        self.image_token              = self.query_processor.image_token
 
-        self.doc_processor = ColModernVBertSparseProcessor(*args, **kwargs)
+        self.query_prefix = getattr(
+            self.query_processor,
+            "query_prefix",
+            self.query_processor.visual_prompt_prefix,
+        )
 
- 
     def process_queries(self, texts: List[str]) -> BatchEncoding:
         return self.query_processor.process_texts(texts)
 
-
     def process_documents(self, images: List[Image.Image]) -> BatchEncoding:
         return self.doc_processor.process_images(images)
-
- 
-    def score(self, qs, ps, device=None, **kwargs):
-        """
-        qs → list of SparseReps or dense vectors from query_encoder
-        ps → list of SparseReps or dense vectors from doc_encoder
-        """
-        return self.doc_processor.score(qs, ps, device=device)
 
     def process_images(self, images: List[Image.Image]) -> BatchEncoding:
         return self.doc_processor.process_images(images)
 
     def process_texts(self, texts: List[str]) -> BatchEncoding:
         return self.query_processor.process_texts(texts)
+    
+    def score(self, qs, ps, device=None, **kwargs):
+        return self.doc_processor.score(qs, ps, device=device)
 
+ 
     def get_n_patches(self, *args, **kwargs):
         return self.doc_processor.get_n_patches(*args, **kwargs)
 
     def get_query_len(self):
-
         if hasattr(self.query_processor, "get_query_len"):
             return self.query_processor.get_query_len()
         raise NotImplementedError
