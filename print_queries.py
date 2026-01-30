04751adb97d571fa7251c67bb3cac6e89a36d4e6
sjoerdgunneweg <sjoerd.gunneweg@gmail.com>
2026-01-30 15:46:12 +0100

    refactor: update model initialization to use SparseModernVBertMLM

--- print_queries.py
+++ print_queries.py
@@ -6,9 +6,10 @@ import numpy as np
 from PIL import Image
 
 from colpali.colpali_engine.models.modernvbert.sparse_mlm.processing_sparsemodernvbert_mlm import SparseModernVBertMLMProcessor
+from colpali.colpali_engine.models.modernvbert.sparse_mlm.modeling_sparsemodernvbert_mlm import SparseModernVBertMLM
 
 processor = SparseModernVBertMLMProcessor.from_pretrained("/home/scur1716/modernvbert/models/sparsemodernvbertmlm_final_checkpoint", trust_remote_code=True)
-model = AutoModel.from_pretrained("/home/scur1716/modernvbert/models/sparsemodernvbertmlm_final_checkpoint", trust_remote_code=True)
+model = SparseModernVBertMLM.from_pretrained("/home/scur1716/modernvbert/models/sparsemodernvbertmlm_final_checkpoint", trust_remote_code=True).eval()
 
 images = [Image.open("photo_28.jpg").convert("RGB"),
           Image.open("photo_27.jpg").convert("RGB")]
@@ -17,7 +18,7 @@ query = [
     "Based on the RadQA context, which cervical spine levels show the most severe degenerative changes?", #NOTE: this is query 28
 ]
 
-text_inputs = processor.process_texts([query]).to(model.device)
+text_inputs = processor.process_texts(query).to(model.device)
 image_inputs = processor.process_images(images).to(model.device)
 
 with torch.no_grad():
