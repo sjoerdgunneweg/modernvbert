2b8a2a70ffef7e0775ba12f8e9ed60f361e093e6
sjoerdgunneweg <sjoerd.gunneweg@gmail.com>
2026-02-04 11:52:37 +0100

    only take in image 28 for print_queries

--- print_queries.py
+++ print_queries.py
@@ -11,8 +11,10 @@ from colpali.colpali_engine.models.modernvbert.sparse_mlm.modeling_sparsemodernv
 processor = SparseModernVBertMLMProcessor.from_pretrained("/home/scur1716/modernvbert/models/sparsemodernvbertmlm_final_checkpoint", trust_remote_code=True)
 model = SparseModernVBertMLM.from_pretrained("/home/scur1716/modernvbert/models/sparsemodernvbertmlm_final_checkpoint", trust_remote_code=True).eval()
 
-images = [Image.open("photo_28.jpg").convert("RGB"),
-          Image.open("photo_27.jpg").convert("RGB")]
+# images = [Image.open("photo_28.jpg").convert("RGB"),
+#           Image.open("photo_27.jpg").convert("RGB")]
+images = [Image.open("photo_28.jpg").convert("RGB")]
+
 
 query = [
     "Based on the RadQA context, which cervical spine levels show the most severe degenerative changes?", #NOTE: this is query 28
@@ -37,3 +39,10 @@ print("tokens with nonzero values in query embedding: ", query_tokens)
 
 doc_tokens = processor.tokenizer.convert_ids_to_tokens(torch.nonzero(doc_embeddings).squeeze()[:, -1].cpu().numpy())
 print("tokens with nonzero values in doc embedding: ", doc_tokens)
+
+
+# TODO check if this puts the embeddings of the two queries together
+# tokens with nonzero values in query embedding:  ['A', 'Ġnew', 'Ġlittle', 'As', 'Ġmain', 'Ġtarget', 'Id', 'ĠThen', 'ips', 'Ġpatient', 'Ġproblems', 'Ġknowledge', 'ĠWar', 'ĠCounty', 'ĠArt', 'Ġoperation', 'Ġactual', 'index', 'Ġfeet', 'Ġfiled', 'Ġmeeting', 'rant', 'çļĦ', 'Ġvisual', 'Ġtransition', 'Ä±', 'Ġfirm', 'ĠChristian', 'Ġreality', 'Ġexcess', 'FS', 'Page', 'ĠMs', 'Ġvoters', 'Ġbasket', 'Ġviewed', 'ĠSteve', 'Ġdestination', 'Ġunexpected', 'Ġmath', 'Ġpoison', 'ĠRick', 'Ġholder', 'Display', 'Ġritual', 'ĠDuncan', 'ĠAsp', 'Ġteens', 'Ġvault', 'Ġbees', 'ĠDrew', 'wang', '510', 'Ġcrawl', 'ĠJiang', 'ĠRaven', 'ĠTales', 'riet', 'Ġtyranny']
+# tokens with nonzero values in doc embedding:  ['%', 'A', 'F', 'X', 'ity', 'Ġdata', 'Ġmodel', 'Ġlittle', 'As', 'Ġtarget', 'Id', 'ips', 'Ġpatient', 'Ġproblems', 'Ġknowledge', 'ĠCounty', 'Ġoperation', 'Ġcomment', 'Ġactual', 'index', 'Ġfeet', 'Ġmeeting', 'Ġtask', 'čĊč', 'çļĦ', 'Ġvisual', 'Ġtransition', 'Ä±', 'Ġfirm', 'Ġreality', 'Ġexcess', 'FS', 'Ġvon', 'Ġcrisis', 'Page', 'ĠMs', 'Ġvoters', 'Ġbasket', 'Ġviewed', 'Ġscattering', 'ĠSteve', 'Ġdestination', 'Ġunexpected', 'Ġmath', 'Ġpoison', 'ĠRick', 'Ġholder', 'Display', 'Ġritual', 'named', 'Lib', 'central', 'ĠDuncan', 'Ġteens', 'Ġvault', 'Ġbees', 'ĠDrew', 'wang', '510', 'ĠMask', 'Ġcrawl', 'ĠJiang', 'ĠRaven', 'Ġboo', 'Ġfollower', 'X', 'ity', 'ft', 'Ġdata', 'Ġlittle', 'Ġmain', 'Ġtarget', 'Ġuser', 'Ġland', 'Ġpatient', 'ĠWar', 'Ġoperation', 'Ġactual', 'Ġfeet', 'Ġtask', 'čĊč', 'çļĦ', 'Ġvisual', 'Ġtransition', 'Ä±', 'Ġfirm', 'Ġexcess', 'Ġvon', 'Page', 'Ġbasket', 'Ġviewed', 'Ġscattering', 'ĠSteve', 'Ġdestination', 'Ġunexpected', 'Ġmath', 'Ġpoison', 'ĠRick', 'named', 'central', 'Ġvault', 'Ġbees', 'ĠDrew', 'wang', '510', 'Ġcrawl', 'ĠJiang', 'ĠRaven', 'Ġboo', 'Ġfollower']
+
+
